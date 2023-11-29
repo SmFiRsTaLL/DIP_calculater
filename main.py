8291a2cf1038e8dbee996763d816620047350104
SmFiRsTaLL <hw44960@horwang.ac.th>
2023-11-29 11:38:36 +0700

    UPDATE1

--- main.py
+++ main.py
@@ -11,8 +11,21 @@ class MyCalculator:
         self.label = tk.Label(self.root, text= "Hello DIP01",font=('Arial',18))
         self.label.pack()
 
-        self.button = tk.Button(self.root, text="click here",height=7)
-        self.button.place(x=20, y=20)
+        self.button = tk.Button(self.root, text="0",height=2,width=8,font=('Arial',9))
+        self.button.place(x=20, y=30)
+
+        self.button = tk.Button(self.root, text="1",height=2,width=8,font=('Arial',9))
+        self.button.place(x=90, y=30)
+
+        self.button = tk.Button(self.root, text="2",height=2,width=8,font=('Arial',9))
+        self.button.place(x=160, y=30)
+
+        self.button = tk.Button(self.root, text="3",height=2,width=8,font=('Arial',9))
+        self.button.place(x=70, y=80)
+
+        self.button = tk.Button(self.root, text="4",height=2,width=8,font=('Arial',9))
+        self.button.place(x=130, y=80)
+
 
         self.root.mainloop()
 
